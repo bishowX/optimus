8fc0e878a3064972e2acce361892d9f1c7f76ddd
bishowX <bishowpandu@gmail.com>
2024-07-08 14:57:24 +0000

    chore: remove default content from editor

--- frontend/src/pages/contents/create.vue
+++ frontend/src/pages/contents/create.vue
@@ -102,20 +102,10 @@ const handleFormSubmit = handleSubmit((values) => {
                             (props) => {
                                 const html = props.editor.getHTML()
                                 setFieldValue('content', props.editor.getText().trim() ? html : '')
-                                console.log(props.editor.getJSON());
-                                
                             }
                         "
                         @blur="componentField.onBlur"
-                        :content="`
-        <p>
-          This is still the text editor you’re used to, but enriched with node views.
-        </p>
-        <vue-component count='0'></vue-component>
-        <p>
-          Did you see that? That’s a Vue component. We are really living in the future.
-        </p>
-      `"
+                        content=""
                     />
                 </FormControl>
                 <FormMessage />
