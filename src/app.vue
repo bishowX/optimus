56ec9fa101e9195ac177c3b8e1add6aa2e119b40
bishowX <bishowpandu@gmail.com>
2024-09-10 20:58:38 +0545

    feat: add navigation progress indicator

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { ref, onMounted, type ComponentInstance } from "vue"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import NavigationIndicator from "@/components/navigation-indicator.vue"
+import { router } from "./router"
+
+const navigationIndicator = ref<ComponentInstance<typeof NavigationIndicator> | null>(null)
+
+onMounted(() => {
+    router.beforeEach((to, from, next) => {
+        navigationIndicator.value?.start()
+        next()
+    })
+
+    router.afterEach(() => {
+        navigationIndicator.value?.finish()
+    })
+})
 </script>
 
 <template>
     <TooltipProvider disableHoverableContent :delay-duration="0">
+        <NavigationIndicator ref="navigationIndicator" />
         <RouterView />
     </TooltipProvider>
 </template>
