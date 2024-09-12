14a60cd2068c6e71f4ef5d1b004791ffd98bb9c2
bishowX <bishowpandu@gmail.com>
2024-09-13 00:06:16 +0545

    add axios and configure api url

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { onMounted } from "vue"
 import { RouterLink } from "vue-router/auto"
+
+onMounted(async () => {
+    console.log(import.meta.env.VITE_API_URL)
+})
 </script>
 
 <template>
