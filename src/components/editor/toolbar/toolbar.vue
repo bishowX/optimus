163df846686fa285e88565469e7250279d253e3e
bishowX <bishow.pandey@ranenetwork.com>
2024-09-16 17:37:31 +0545

    fix scrollbar

--- src/components/editor/toolbar/toolbar.vue
+++ src/components/editor/toolbar/toolbar.vue
@@ -41,7 +41,7 @@ const makeChart = () => {
 </script>
 
 <template>
-    <div class="w-full flex items-center gap-2 overflow-x-scroll sticky -top-2 z-10">
+    <div class="w-full flex items-center gap-2 overflow-x-auto sticky -top-2 z-10">
         <Heading :editor="editor" />
         <Separator class="h-6" orientation="vertical" />
         <div class="flex items-center gap-2">
