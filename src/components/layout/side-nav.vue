a8cf95784c190c61e123a511abc232952aad3f3f
bishowX <bishow.pandey@ranenetwork.com>
2024-10-03 17:42:40 +0545

    revamp ui

--- src/components/layout/side-nav.vue
+++ src/components/layout/side-nav.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
+import { ref } from "vue"
 import { RouterLink } from "vue-router"
+import { ChevronDown } from "lucide-vue-next"
 import { Icon } from "@iconify/vue"
-import { cn } from "@/lib/utils"
-import { Button, buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
 import { Separator } from "@/components/ui/separator"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+import SidenavItem from "./sidenav-item.vue"
 
 export interface LinkProp {
     title: string
@@ -19,6 +23,9 @@ interface NavProps {
 }
 
 defineProps<NavProps>()
+
+const isOpen = ref(false)
+const isOpen2 = ref(false)
 
 const emits = defineEmits<{
     (e: "toggleCollapse"): void
@@ -59,76 +66,126 @@ const emits = defineEmits<{
 
         <Separator orientation="horizontal" />
 
-        <div class="flex flex-col gap-1" :class="isCollapsed ? 'px-1' : 'px-2'">
-            <template v-for="(link, index) of links">
-                <Tooltip v-if="isCollapsed" :key="`1-${index}`" :delay-duration="0">
-                    <TooltipTrigger>
-                        <RouterLink
-                            custom
-                            :to="link.link"
-                            v-slot="{ isExactActive: isActive, href, navigate }"
-                        >
-                            <a
-                                :href="href"
-                                @click="navigate"
-                                v-bind="$attrs"
-                                :class="
-                                    cn(
-                                        'h-9 w-9',
-                                        isActive
-                                            ? buttonVariants({ variant: 'default', size: 'icon' })
-                                            : buttonVariants({ variant: 'ghost', size: 'icon' }),
-                                        isActive &&
-                                            'dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white',
-                                    )
-                                "
-                            >
-                                <Icon :icon="link.icon" class="size-5" />
-                                <span class="sr-only">{{ link.title }}</span>
-                            </a>
-                        </RouterLink>
-                    </TooltipTrigger>
-                    <TooltipContent side="right" class="flex items-center gap-4">
-                        {{ link.title }}
-                        <span v-if="link.label" class="ml-auto text-muted-foreground">
-                            {{ link.label }}
-                        </span>
-                    </TooltipContent>
-                </Tooltip>
-
-                <RouterLink
-                    :key="`2-${index}`"
-                    v-else
-                    custom
-                    :to="link.link"
-                    v-slot="{ isExactActive: isActive, href, navigate }"
-                >
-                    <a
-                        v-bind="$attrs"
-                        :href="href"
-                        @click="navigate"
-                        :class="
-                            cn(
-                                isActive
-                                    ? buttonVariants({ variant: 'default', size: 'sm' })
-                                    : buttonVariants({ variant: 'ghost', size: 'sm' }),
-                                'justify-start',
-                                isActive &&
-                                    'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white',
-                            )
-                        "
+        <div class="flex flex-col gap-3" :class="isCollapsed ? 'items-center px-1' : 'px-2'">
+            <div v-if="isCollapsed">
+                <HoverCard :open-delay="0" :close-delay="0">
+                    <HoverCardTrigger>
+                        <Button size="icon" variant="ghost"><Icon icon="lucide:wrench" /></Button>
+                    </HoverCardTrigger>
+                    <HoverCardContent class="p-1" align="center" side="right">
+                        <div class="flex flex-col gap-1">
+                            <SidenavItem
+                                icon="lucide:file-search-2"
+                                link="/contents/create"
+                                title="Propriety content"
+                            />
+                            <SidenavItem
+                                icon="lucide:file-search"
+                                label="99"
+                                link="/about"
+                                title="Data visualization"
+                            />
+                            <SidenavItem icon="lucide:activity" link="/overview" title="Event" />
+                            <SidenavItem
+                                icon="lucide:airplay"
+                                link="/contents"
+                                title="Other content"
+                            />
+                        </div>
+                    </HoverCardContent>
+                </HoverCard>
+            </div>
+            <Collapsible v-else v-model:open="isOpen">
+                <CollapsibleTrigger as-child>
+                    <Button
+                        variant="ghost"
+                        size="sm"
+                        class="w-full justify-between transition-all duration-300"
                     >
-                        <Icon :icon="link.icon" class="mr-2 size-5" />
-                        {{ link.title }}
-                        <span
-                            v-if="link.label"
-                            :class="cn('ml-auto', isActive && 'text-background dark:text-white')"
-                        >
-                            {{ link.label }}
-                        </span>
-                    </a>
-                </RouterLink>
-            </template>
+                        Manage Content
+                        <ChevronDown
+                            class="transition-all duration-300"
+                            :class="isOpen ? 'rotate-180' : ''"
+                        />
+                    </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent class="mt-1 flex flex-col gap-1 pl-4">
+                    <SidenavItem
+                        icon="lucide:file-search-2"
+                        link="/contents/create"
+                        title="Propriety content"
+                    />
+                    <SidenavItem
+                        icon="lucide:file-search"
+                        label="99"
+                        link="/401"
+                        title="Data visualization"
+                    />
+                    <SidenavItem icon="lucide:activity" link="400" title="Event" />
+                    <SidenavItem icon="lucide:airplay" link="404" title="Other content" />
+                </CollapsibleContent>
+            </Collapsible>
+
+            <div v-if="isCollapsed">
+                <HoverCard :open-delay="0" :close-delay="0">
+                    <HoverCardTrigger>
+                        <Button size="icon" variant="ghost"><Icon icon="lucide:air-vent" /></Button>
+                    </HoverCardTrigger>
+                    <HoverCardContent class="flex flex-col gap-1 p-1" align="center" side="right">
+                        <SidenavItem
+                            icon="lucide:alarm-clock"
+                            link="/contents/create1"
+                            title="Monitoring"
+                        />
+                        <SidenavItem
+                            icon="lucide:alarm-smoke"
+                            label="69"
+                            link="/contents/create2"
+                            title="Brief builder"
+                        />
+                        <SidenavItem
+                            icon="lucide:sailboat"
+                            link="/contents/create3"
+                            title="Add story"
+                        />
+                    </HoverCardContent>
+                </HoverCard>
+            </div>
+            <Collapsible v-else v-model:open="isOpen2">
+                <CollapsibleTrigger as-child>
+                    <Button
+                        variant="ghost"
+                        size="sm"
+                        class="w-full justify-between transition-all duration-300"
+                    >
+                        Monitoring
+                        <ChevronDown
+                            class="transition-all duration-300"
+                            :class="isOpen2 ? 'rotate-180' : ''"
+                        />
+                    </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent class="mt-1 pl-4">
+                    <div class="flex flex-col gap-1">
+                        <SidenavItem
+                            icon="lucide:alarm-clock"
+                            link="/contents/create4"
+                            title="Monitoring"
+                        />
+                        <SidenavItem
+                            icon="lucide:alarm-smoke"
+                            label="69"
+                            link="/contents/create5"
+                            title="Brief builder"
+                        />
+                        <SidenavItem
+                            icon="lucide:sailboat"
+                            link="/contents/create6"
+                            title="Add story"
+                        />
+                    </div>
+                </CollapsibleContent>
+            </Collapsible>
         </div>
     </aside>
 </template>
---
+++ src/components/layout/sidenav-item.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils"
+import { RouterLink } from "vue-router"
+import { buttonVariants } from "@/components/ui/button"
+import { Icon } from "@iconify/vue"
+
+defineProps<{
+    title: string
+    label?: string
+    icon: string
+    link: string
+}>()
+</script>
+<template>
+    <RouterLink custom :to="link" v-slot="{ isExactActive: isActive, href, navigate }">
+        <a
+            v-bind="$attrs"
+            :href="href"
+            @click="navigate"
+            :class="
+                cn(
+                    isActive
+                        ? buttonVariants({ variant: 'default', size: 'sm' })
+                        : buttonVariants({ variant: 'ghost', size: 'sm' }),
+                    'justify-start',
+                    isActive &&
+                        'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white',
+                )
+            "
+        >
+            <Icon :icon="icon" class="mr-2 size-5" />
+            {{ title }}
+            <span
+                v-if="label"
+                :class="cn('ml-auto', isActive && 'text-background dark:text-white')"
+            >
+                {{ label }}
+            </span>
+        </a>
+    </RouterLink>
+</template>
